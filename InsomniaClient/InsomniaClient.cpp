bfb091ef46ea9b0681d89011056683e506f1342a
Focusucof <imthefirstfocus@gmail.com>
2021-10-02 15:08:58 -0400

    pre-release v0.03

--- InsomniaClient/InsomniaClient.cpp
+++ InsomniaClient/InsomniaClient.cpp
@@ -1,6 +1,3 @@
-// ApertureClient.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include "httplib.h"
 #include "LocalRiotClient.h"
