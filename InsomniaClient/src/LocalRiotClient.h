ce3ddd10054dc4330b893648286470177bc7cb7e
Focusucof <imthefirstfocus@gmail.com>
2021-10-05 16:35:06 -0400

    fixed memory leak and cleaned up file structure

---
+++ InsomniaClient/src/LocalRiotClient.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <curl/curl.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <stdlib.h>
+#include <sstream>
+#include <algorithm>
+#include "../include/base64encode.h"
+#include "../include/json.h"
+
+class LocalRiotClient {
+
+	public:
+		LocalRiotClient();
+		~LocalRiotClient();
+
+	public:
+		bool bIsGameOpen();
+		std::vector<std::string> parseLockFile();
+		std::vector<std::string> getCredentials();
+
+	private:
+		//CURL* curl;
+
+};
+
